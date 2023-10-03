f1c1e43b08476305cfaf08327edca6e677481d32
root <root@NIN405.DOMNAVINSA.NET>
2023-10-03 14:45:23 -0600

    Add script to upload files to google drive

---
+++ google-api/google-drive/google-drive-upload.py
@@ -0,0 +1,37 @@
+
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from pydrive2.files import FileNotUploadedError
+
+directorio_credenciales = 'credentials_module.json'
+
+# INICIAR SESION
+def login():
+    GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = directorio_credenciales
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile(directorio_credenciales)
+    
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth(port_numbers=[8092])
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+        
+    gauth.SaveCredentialsFile(directorio_credenciales)
+    credenciales = GoogleDrive(gauth)
+    return credenciales
+
+# SUBIR UN ARCHIVO A DRIVE
+def subir_archivo(ruta_archivo,id_folder):
+    credenciales = login()
+    archivo = credenciales.CreateFile({'parents': [{"kind": "drive#fileLink",\
+                                                    "id": id_folder}]})
+    archivo['title'] = ruta_archivo.split("/")[-1]
+    archivo.SetContentFile(ruta_archivo)
+    archivo.Upload()
+
+if __name__ == "__main__":
+    ruta_archivo = '/opt/googleApi/Python_and_PyDrive2/HelloWorld-V2.txt'
+    id_folder = '1c_zdW-kBL7DvFuGgJGM9P6Nbgka5tBfx'
+    subir_archivo(ruta_archivo,id_folder)
